6d10dad16ad35729cc81610a4d9094b4f402a5c1
LamcatUK <git@lamcat.co.uk>
2025-05-24 17:52:25 +0100

    cta

--- page.php
+++ page.php
@@ -14,9 +14,7 @@ get_header();
     the_post();
     the_content();
 
-    if ( ! is_front_page() ) {
-        get_template_part( 'page-templates/blocks/lc-cta' );
-    }
+    get_template_part( 'page-templates/blocks/lc-cta' );
     ?>
 </main>
 <?php
