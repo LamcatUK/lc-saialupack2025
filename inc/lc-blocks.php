c3d135f672b3e201810034f3ac18c40281489854
LamcatUK <git@lamcat.co.uk>
2025-07-15 20:38:57 +0100

    lc child pages

--- inc/lc-blocks.php
+++ inc/lc-blocks.php
@@ -24,6 +24,23 @@ function acf_blocks() {
 
         acf_register_block_type(
             array(
+                'name'            => 'lc_child_pages',
+                'title'           => __( 'LC Child Pages' ),
+                'category'        => 'layout',
+                'icon'            => 'cover-image',
+                'render_template' => 'page-templates/blocks/lc-child-pages.php',
+                'mode'            => 'edit',
+                'supports'        => array(
+                    'mode'      => false,
+                    'anchor'    => true,
+                    'className' => true,
+                    'align'     => true,
+                ),
+            )
+        );
+
+        acf_register_block_type(
+            array(
                 'name'            => 'lc_faqs',
                 'title'           => __( 'LC FAQs' ),
                 'category'        => 'layout',
