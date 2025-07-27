47982ba2c550582e2b7e5254ef257a54563b99f2
LamcatUK <git@lamcat.co.uk>
2025-07-27 12:18:19 +0100

    pdf download

---
+++ page-templates/blocks/lc-pdf-download.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Block template for LC PDF Download.
+ *
+ * @package lc-saialupack2025
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+$classes = $block['className'] ?? 'py-5';
+
+$pdf_id  = get_field( 'pdf_file' );
+$pdf_url = $pdf_id ? wp_get_attachment_url( $pdf_id ) : '';
+?>
+<section class="pdf_download <?= esc_attr( $classes ); ?>">
+	<div class="container">
+		<div class="row g-5">
+			<div class="col-md-8">
+				<?= wp_kses_post( get_field( 'pdf_download_text' ) ); ?>
+				<a href="<?= esc_url( $pdf_url ); ?>" class="button button-primary">Download PDF</a>
+			</div>
+			<div class="col-md-4">
+				<?php
+				if ( wp_attachment_is( 'pdf', $pdf_id ) ) {
+					$thumb_id = get_post_thumbnail_id( $pdf_id );
+					if ( $thumb_id ) {
+						echo wp_get_attachment_image( $thumb_id, 'full', false, array( 'class' => 'img-fluid' ) );
+					} else {
+						// Try to get PDF preview.
+						$pdf_image = wp_get_attachment_image( $pdf_id, 'full', true, array( 'class' => 'img-fluid' ) );
+						if ( $pdf_image ) {
+							echo $pdf_image;
+						}
+					}
+				}
+                ?>
+			</div>
+		</div>
+	</div>
+</section>
