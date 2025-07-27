64a6e6401551657bccf5909153e482ae37313c02
LamcatUK <git@lamcat.co.uk>
2025-07-27 12:24:19 +0100

    pdf download

--- page-templates/blocks/lc-pdf-download.php
+++ page-templates/blocks/lc-pdf-download.php
@@ -17,7 +17,7 @@ $pdf_url = $pdf_id ? wp_get_attachment_url( $pdf_id ) : '';
 		<div class="row g-5">
 			<div class="col-md-8">
 				<?= wp_kses_post( get_field( 'pdf_download_text' ) ); ?>
-				<a href="<?= esc_url( $pdf_url ); ?>" class="button button-primary">Download PDF</a>
+				<a href="<?= esc_url( $pdf_url ); ?>" target="_blank" class="button button-primary mt-4">Download PDF</a>
 			</div>
 			<div class="col-md-3 offset-md-1">
 				<?php
@@ -25,14 +25,14 @@ $pdf_url = $pdf_id ? wp_get_attachment_url( $pdf_id ) : '';
 					$thumb_id = get_post_thumbnail_id( $pdf_id );
 					if ( $thumb_id ) {
 						?>
-				<a href="<?= esc_url( $pdf_url ); ?>"><?= wp_get_attachment_image( $thumb_id, 'full', false, array( 'class' => 'img-fluid' ) ); ?></a>
+				<a href="<?= esc_url( $pdf_url ); ?>" target="_blank"><?= wp_get_attachment_image( $thumb_id, 'full', false, array( 'class' => 'img-fluid' ) ); ?></a>
 						<?php
 					} else {
 						// Try to get PDF preview.
 						$pdf_image = wp_get_attachment_image( $pdf_id, 'full', true, array( 'class' => 'img-fluid' ) );
 						if ( $pdf_image ) {
 							?>
-				<a href="<?= esc_url( $pdf_url ); ?>"><?= wp_kses_post( $pdf_image ); ?></a>
+				<a href="<?= esc_url( $pdf_url ); ?>" target="_blank"><?= wp_kses_post( $pdf_image ); ?></a>
 							<?php
 						}
 					}
